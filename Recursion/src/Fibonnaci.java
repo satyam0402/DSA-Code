850f37c7df3dc745593e8814ae03834236cf6333
Satyam Rajput <91290831+satyam0402@users.noreply.github.com>
2024-06-19 15:08:11 -0500

    Add files via upload

---
+++ Recursion/src/Fibonnaci.java
@@ -0,0 +1,14 @@
+public class Fibonnaci {
+    public static void main(String[] args){
+        int ans = fibo(4);
+        System.out.print(ans);
+    }
+    static int fibo( int n){
+        //base condition
+        if ( n < 2){
+            return n;
+        }
+        return fibo ( n-1) + fibo (n-2);
+    }
+
+}
---
+++ Recursion/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //E1. Function to WRITE HELLO WORLD 5 TIMES USING RECURSION ( Basic concept debug to understand )
+        message();
+    }
+    static void message(){
+        System.out.println("Hello World");
+        message1();
+
+    }
+    static void message1(){
+        System.out.println("Hello World");
+        message2();
+
+    }
+    static void message2(){
+        System.out.println("Hello World");
+        message3();
+
+    }
+    static void message3(){
+        System.out.println("Hello World");
+        message4();
+
+    }
+    static void message4(){
+        System.out.println("Hello World");
+
+    }
+}
