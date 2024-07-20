2966d521d0920416f893dab82869615cc0aec780
Satyam Rajput <91290831+satyam0402@users.noreply.github.com>
2024-07-20 13:26:54 -0500

    Add files via upload

---
+++ Recursion STRINGS/SubseqAscii.java
@@ -0,0 +1,18 @@
+public class SubseqAscii {
+    public static void main(String[] args) {
+        subseqascii("", "abc");
+
+
+
+    }
+    static void subseqascii(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subseqascii(p + ch, up.substring(1));
+        subseqascii(p,up.substring(1));
+        subseqascii(p + (ch+0), up.substring(1));
+    }
+}
