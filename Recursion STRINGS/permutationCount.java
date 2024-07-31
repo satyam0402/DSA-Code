bc8c1146f129dd146bcf30ccec49994081103939
Satyam Rajput <91290831+satyam0402@users.noreply.github.com>
2024-07-31 18:47:47 -0500

    Add files via upload

---
+++ Recursion STRINGS/permutationCount.java
@@ -0,0 +1,19 @@
+public class permutationCount {
+    public static void main(String[] args) {
+        System.out.println(CountPermutations("", "abc"));
+
+    }
+    static int CountPermutations(String p, String up){
+        if(up.isEmpty()){
+            return 1;
+        }
+        int count = 0;
+        char ch = up.charAt(0);
+        for(int i = 0; i<= p.length(); i++){
+            String f = p.substring(0,i);
+            String s = p.substring(i, p.length());
+            count = count + CountPermutations(f + ch + s, up.substring(1));
+        }
+        return count;
+    }
+}
